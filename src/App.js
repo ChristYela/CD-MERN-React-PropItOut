33e149ad30a0d532634a63ca61e11b4d05b2eee5
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2022-10-03 17:44:06 -0500

    Prop it out exercise MERN - React
    Prop it out exercise MERN - React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import PersonCard from './components/PersonCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <PersonCard lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"} />
+        <PersonCard lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"} />
+        <PersonCard lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"} />
+        <PersonCard lastName={"Smith"} firstName={"Maria"} age={62} hairColor={"Brown"} />
+    </div>
+  );
+}
+
+export default App;
